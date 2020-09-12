57dc69f963492fbe95c3dc6a5e800cf2d01956b5
Sherilyn Michael <sherimichael@gmail.com>
2020-09-12 07:57:00 -0400

    added annotations to all models but LoginDTO

---
+++ src/main/java/com/revature/models/Games.java
@@ -0,0 +1,134 @@
+package com.revature.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="games")
+public class Games {
+	
+	@Column(name="char_id", unique = true, nullable = false)
+	private int cId;//character id
+	@Column(name="char_name", unique = true, nullable = false)
+	private String cName;//character name
+	@Column(name="quote")
+	private String quote;
+	@Column(name="fun_fact1")
+	private String funFact1;
+	@Column(name="fun_fact2")
+	private String funFact2;
+	
+	public Games() {
+		super();
+	}
+
+	public Games(int cId, String cName, String quote, String funFact1, String funFact2) {
+		super();
+		this.cId = cId;
+		this.cName = cName;
+		this.quote = quote;
+		this.funFact1 = funFact1;
+		this.funFact2 = funFact2;
+	}
+
+	public Games(String cName, String quote, String funFact1, String funFact2) {
+		super();
+		this.cName = cName;
+		this.quote = quote;
+		this.funFact1 = funFact1;
+		this.funFact2 = funFact2;
+	}
+
+	public int getcId() {
+		return cId;
+	}
+
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+
+	public String getcName() {
+		return cName;
+	}
+
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+
+	public String getQuote() {
+		return quote;
+	}
+
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+
+	public String getFunFact1() {
+		return funFact1;
+	}
+
+	public void setFunFact1(String funFact1) {
+		this.funFact1 = funFact1;
+	}
+
+	public String getFunFact2() {
+		return funFact2;
+	}
+
+	public void setFunFact2(String funFact2) {
+		this.funFact2 = funFact2;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cId;
+		result = prime * result + ((cName == null) ? 0 : cName.hashCode());
+		result = prime * result + ((funFact1 == null) ? 0 : funFact1.hashCode());
+		result = prime * result + ((funFact2 == null) ? 0 : funFact2.hashCode());
+		result = prime * result + ((quote == null) ? 0 : quote.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Games other = (Games) obj;
+		if (cId != other.cId)
+			return false;
+		if (cName == null) {
+			if (other.cName != null)
+				return false;
+		} else if (!cName.equals(other.cName))
+			return false;
+		if (funFact1 == null) {
+			if (other.funFact1 != null)
+				return false;
+		} else if (!funFact1.equals(other.funFact1))
+			return false;
+		if (funFact2 == null) {
+			if (other.funFact2 != null)
+				return false;
+		} else if (!funFact2.equals(other.funFact2))
+			return false;
+		if (quote == null) {
+			if (other.quote != null)
+				return false;
+		} else if (!quote.equals(other.quote))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Games [cId=" + cId + ", cName=" + cName + ", quote=" + quote + ", funFact1=" + funFact1 + ", funFact2="
+				+ funFact2 + "]";
+	}
+}
--- src/main/java/com/revature/models/LoginDTO.java
+++ src/main/java/com/revature/models/LoginDTO.java
@@ -6,17 +6,11 @@ public class LoginDTO {
 	
 	public LoginDTO() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public LoginDTO(String username, String password) {
 		super();
 		this.username = username;
 		this.password = password;
-	}
-	
-	
-	
-	
-	
+	}	
 }
