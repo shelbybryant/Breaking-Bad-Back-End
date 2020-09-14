ae210536afc82fe05c32c281d6cbff621f823703
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2020-09-14 15:24:40 -0400

    took out game question fields from games model, updated the sql scripts
    realized we do not need the question fields, since it can be done in the front end and it didn't like our huge constructor anyway.
    Our table will not have a lot of info but that's okay, this is more simple.

--- src/main/java/com/revature/daos/IGamesDAO.java
+++ src/main/java/com/revature/daos/IGamesDAO.java
@@ -10,8 +10,9 @@ public interface IGamesDAO {
 	public List<Games> findByUserId(int userId);
 	public boolean addGame (Games games);
 	
-	//I don't know about the random quotes method because the api has functionality for that
-	/* And we don't need the quote by character because the api will also do that. Our QuotesDAO
+	/*I don't know about the random quotes method because the api has functionality for that and we will just put
+	 * it into our question field
+	* And we don't need the quote by character because the api will also do that. Our QuotesDAO
 	 * will have the methodology for storing and then retrieving those saved quotes
 	 */
 	
