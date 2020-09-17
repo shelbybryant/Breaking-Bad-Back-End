59e809f9c93dbc31f3ef9cd2d9e5990a5fe01434
Sherilyn Michael <sherimichael@gmail.com>
2020-09-16 22:30:13 -0400

    added Spr Annotations to UserDao

--- src/main/java/com/revature/daos/UserDAO.java
+++ src/main/java/com/revature/daos/UserDAO.java
@@ -2,14 +2,21 @@ package com.revature.daos;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.revature.models.Users;
 
+@Repository
+@Transactional
 public class UserDAO implements IUserDAO {
 	private static final Logger log = LogManager.getLogger(UserDAO.class);
 	
+	private SessionFactory sf;
+	
+	@Autowired
 	public UserDAO() {
 		super();
 	}
