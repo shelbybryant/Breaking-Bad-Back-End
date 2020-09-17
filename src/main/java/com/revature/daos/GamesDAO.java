1fabea6eed5941af6024d053c5b578e5e11f6575
Sherilyn Michael <sherimichael@gmail.com>
2020-09-16 23:33:10 -0400

    added Spr annotations to GamesDao

--- src/main/java/com/revature/daos/GamesDAO.java
+++ src/main/java/com/revature/daos/GamesDAO.java
@@ -2,18 +2,29 @@ package com.revature.daos;
 
 import java.util.List;
 
-import com.revature.models.Games;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.Games;
+import com.revature.models.Users;
+
+@Repository
+@Transactional
 public class GamesDAO implements IGamesDAO{
+	
+	SessionFactory sf;
 
 	@Override
 	public List<Games> findByUserId(int userId) {
-		return null;
+		Session ses = sf.getCurrentSession();
+		return ses.get(Users.class, userId);
 	}
 
 	@Override
-	public boolean addGame(Games games) {
-		return false;
+	public void addGame(Games games) {
+		Session ses = sf.getCurrentSession();
+		ses.merge(games);
 	}
 
 }
